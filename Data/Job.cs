7d4c1aecc907c8209245684a7acd7586964bdb15
ovadia <info@webmax.co.il>
2021-06-08 10:56:19 +0300

    quartz working blazor service

---
+++ Data/Job.cs
@@ -0,0 +1,20 @@
+﻿using Quartz;
+using System;
+using System.Threading.Tasks;
+
+namespace SchedularPrac
+{
+    public class Job : IJob
+    {
+        public Task Execute(IJobExecutionContext context)
+        {
+            Console.WriteLine("My debug output."+DateTime.UtcNow);
+            return null;
+        }
+
+        /*Task IJob.Execute(IJobExecutionContext context)
+        {
+            throw new NotImplementedException( );
+        }*/
+    }
+}
---
+++ Data/Schedular.cs
@@ -0,0 +1,32 @@
+﻿using Quartz;
+using Quartz.Impl;
+using System;
+using Microsoft.AspNetCore.Components;
+using System.Threading;
+
+namespace SchedularPrac
+{
+    public class Scheduler
+    {
+        //[Inject] public ISchedulerFactory _scd { get; set; }
+        public async System.Threading.Tasks.Task Start(DateTime date)
+
+        {
+            
+            IScheduler scheduler = await StdSchedulerFactory.GetDefaultScheduler( );
+
+            scheduler.Start( );
+            IJobDetail job = JobBuilder.Create<Job>( ).Build( );
+            ITrigger trigger = TriggerBuilder.Create( )
+             .WithIdentity("IDGJob", "IDG")
+             .StartNow( )
+                .WithSimpleSchedule(x => x
+                    .WithIntervalInSeconds(15)
+                     .RepeatForever( ))
+                        .Build( );
+            scheduler.ScheduleJob(job, trigger);
+
+        }
+
+    }
+}
